eed60ca81421a5adacbdab1a81dd2dd45c6c299a
Sebastian Fuentes Chavez <sebas_alonso2001@hotmail.com>
2025-02-10 15:05:43 -0500

    cambio con web service

--- Areas/Cita/Data/PacienteData.cs
+++ Areas/Cita/Data/PacienteData.cs
@@ -1,41 +1,60 @@
 ﻿using CLINICA_API.Areas.General.Helpers;
 using CLINICA_API.Modelo;
 using Dapper;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
 using static CLINICA_API.Areas.General.Helpers.ServiceConnection;
 
 namespace CLINICA_API.Areas.Cita.Data
 {
     public class PacienteData
     {
-        private readonly ServiceConnection _connection;
-        public PacienteData(ServiceConnection connection)
+        private string urlws;
+        //private readonly ServiceConnection _connection;
+        public PacienteData(IConfiguration configuration/*ServiceConnection connection*/)
         {
-            _connection = connection;
+            //_connection = connection;
+            urlws = configuration.GetConnectionString("ConnecionSigeURLWS") + "Clinica/ServiceClinica.svc/";
         }
 
+        WebClient cliente = new WebClient() { Encoding = Encoding.UTF8 };
         public MensajeJson ListarPacientexFiltro(string filtro)
         {
-            var parameters = new DynamicParameters();
-            parameters.Add("@filtro", filtro);
-            return _connection.MetodoDatatabletostringsql("Clinica.sp_listar_pacientexfiltro", parameters, TipoConexion.SIGE);
+            var json = cliente.DownloadString(urlws + "paciente/listar_pacientexfiltro/" + filtro);
+            var jsonobj = JsonConvert.DeserializeObject(json);
+            return new MensajeJson("OK", jsonobj.ToString());
+            //var parameters = new DynamicParameters();
+            //parameters.Add("@filtro", filtro);
+            //return _connection.MetodoDatatabletostringsql("Clinica.sp_listar_pacientexfiltro", parameters, TipoConexion.SIGE);
         }
         public MensajeJson ObtenerPacientexNumDocumento(string numdocumento)
         {
-            var parameters = new DynamicParameters();
-            parameters.Add("@numdocumento", numdocumento);
-            return _connection.MetodoDatatabletostringsql("Clinica.sp_obtener_pacientexnumdocumento", parameters, TipoConexion.SIGE);
+            var json = cliente.DownloadString(urlws + "paciente/obtener_pacientexnumdocumento/" + numdocumento);
+            var jsonobj = JsonConvert.DeserializeObject(json);
+            return new MensajeJson("OK", jsonobj.ToString());
+            //var parameters = new DynamicParameters();
+            //parameters.Add("@numdocumento", numdocumento);
+            //return _connection.MetodoDatatabletostringsql("Clinica.sp_obtener_pacientexnumdocumento", parameters, TipoConexion.SIGE);
         }
         public MensajeJson ObtenerPacientexIdPaciente(string idpaciente)
         {
-            var parameters = new DynamicParameters();
-            parameters.Add("@idpaciente", idpaciente);
-            return _connection.MetodoDatatabletostringsql("Clinica.sp_obtener_pacientexidpaciente", parameters, TipoConexion.SIGE);
+            var json = cliente.DownloadString(urlws + "paciente/obtener_pacientexidpaciente/" + idpaciente);
+            var jsonobj = JsonConvert.DeserializeObject(json);
+            return new MensajeJson("OK", jsonobj.ToString());
+            //var parameters = new DynamicParameters();
+            //parameters.Add("@idpaciente", idpaciente);
+            //return _connection.MetodoDatatabletostringsql("Clinica.sp_obtener_pacientexidpaciente", parameters, TipoConexion.SIGE);
         }
         public MensajeJson GuardarEditarPaciente(string jsonpaciente)
         {
-            var parameters = new DynamicParameters();
-            parameters.Add("@jsonpaciente", jsonpaciente);
-            return _connection.MetodoRespuestasql("Clinica.sp_registrar_actualizar_paciente", parameters, 50, TipoConexion.SIGE);
+            string jsonenviar = JsonConvert.SerializeObject(jsonpaciente);
+            cliente.Headers["Content-Type"] = "application/json";
+            var respuesta = cliente.UploadString(urlws + "paciente/registrar_paciente", "POST", jsonenviar);
+            return new MensajeJson("OK", respuesta.ToString());
+            //var parameters = new DynamicParameters();
+            //parameters.Add("@jsonpaciente", jsonpaciente);
+            //return _connection.MetodoRespuestasql("Clinica.sp_registrar_actualizar_paciente", parameters, 50, TipoConexion.SIGE);
         }
     }
 }
--- Areas/General/Data/ClienteData.cs
+++ Areas/General/Data/ClienteData.cs
@@ -1,35 +1,52 @@
 ﻿using CLINICA_API.Areas.General.Helpers;
 using CLINICA_API.Modelo;
 using Dapper;
+using Newtonsoft.Json;
+using System.Data;
+using System.Net;
+using System.Text;
 using static CLINICA_API.Areas.General.Helpers.ServiceConnection;
 
 namespace CLINICA_API.Areas.General.Data
 {
     public class ClienteData
     {
-        private readonly ServiceConnection _connection;
-        public ClienteData(ServiceConnection connection)
+        private string urlws;
+        //private readonly ServiceConnection _connection;
+        public ClienteData(IConfiguration configuration/*ServiceConnection connection*/)
         {
-            _connection = connection;
+            //_connection = connection;
+            urlws = configuration.GetConnectionString("ConnecionSigeURLWS") + "Clinica/ServiceClinica.svc/";
         }
 
+        WebClient cliente = new WebClient() { Encoding = Encoding.UTF8 };
         public MensajeJson ObtenerClientexNumDocumento(string numdocumento)
         {
-            var parameters = new DynamicParameters();
-            parameters.Add("@numdocumento", numdocumento);
-            return _connection.MetodoDatatabletostringsql("Clinica.sp_obtener_clientexnumdocumento", parameters, TipoConexion.SIGE);
+            var json = cliente.DownloadString(urlws + "cliente/obtener_clientexnumdocumento/" + numdocumento);
+            var jsonobj = JsonConvert.DeserializeObject(json);
+            return new MensajeJson("OK", jsonobj.ToString());
+            //var parameters = new DynamicParameters();
+            //parameters.Add("@numdocumento", numdocumento);
+            //return _connection.MetodoDatatabletostringsql("Clinica.sp_obtener_clientexnumdocumento", parameters, TipoConexion.SIGE);
         }
         public MensajeJson GuardarEditarCliente(string jsoncliente)
         {
-            var parameters = new DynamicParameters();
-            parameters.Add("@jsoncliente", jsoncliente);
-            return _connection.MetodoRespuestasql("Clinica.sp_registrar_actualizar_cliente", parameters, 50, TipoConexion.SIGE);
+            string jsonenviar = JsonConvert.SerializeObject(jsoncliente);
+            cliente.Headers["Content-Type"] = "application/json";
+            var respuesta = cliente.UploadString(urlws + "cliente/registrar_cliente", "POST", jsonenviar);
+            return new MensajeJson("OK", respuesta.ToString());
+            //var parameters = new DynamicParameters();
+            //parameters.Add("@jsoncliente", jsoncliente);
+            //return _connection.MetodoRespuestasql("Clinica.sp_registrar_actualizar_cliente", parameters, 50, TipoConexion.SIGE);
         }
         public MensajeJson ObtenerClientexIdCliente(string idcliente)
         {
-            var parameters = new DynamicParameters();
-            parameters.Add("@idcliente", idcliente);
-            return _connection.MetodoDatatabletostringsql("Clinica.sp_obtener_clientexidcliente", parameters, TipoConexion.SIGE);
+            var json = cliente.DownloadString(urlws + "cliente/obtener_clientexidcliente/" + idcliente);
+            var jsonobj = JsonConvert.DeserializeObject(json);
+            return new MensajeJson("OK", jsonobj.ToString());
+            //var parameters = new DynamicParameters();
+            //parameters.Add("@idcliente", idcliente);
+            //return _connection.MetodoDatatabletostringsql("Clinica.sp_obtener_clientexidcliente", parameters, TipoConexion.SIGE);
         }
     }
 }
